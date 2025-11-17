99e53ddb5a33c15a6d31db4d6f3556b44f0e8187
Varshini690 <varshinimanchikalapudi@gmail.com>
2025-11-17 21:10:00 +0530

    Backend ready for Render deploy

--- backend/mockbackend/settings.py
+++ backend/mockbackend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-g9_vz!zxd33%*u!vq-#&_18__$k^!7l2tdnu*%8f0qt618@5yi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
